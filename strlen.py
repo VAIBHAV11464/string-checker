75f33c58ebafd579814569a377eca56cf0fc0ec8
VAIBHAV11464 <vaibhav11464@gmail.com>
2026-08-05 16:16:55 +0530

    first commit

---
+++ strlen.py
@@ -0,0 +1,15 @@
+def count_vowels(s):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in s:
+        if char in vowels:
+            count += 1
+    return count
+
+if __name__ == "__main__":
+    test_string = "vscode"
+    test_string2 = "giithub"
+    result = count_vowels(test_string)
+    result_2 = count_vowels(test_string2)
+    print(f"The number of vowels in '{test_string}' is: {result}")
+    print(f"The number of vowels in '{test_string2}' is: {result_2}")
